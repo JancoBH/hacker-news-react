f06706d96347da199f58332267d465d42b1ee5d1
JancoBH <jancoboscan@gmail.com>
2022-08-15 02:19:10 -0300

    Fixing Axios Interceptor bug after build

--- src/services/AxiosProvider.tsx
+++ src/services/AxiosProvider.tsx
@@ -1,33 +1,30 @@
 import axios from 'axios';
 import { API_URL, INews } from '../models';
-import { useEffect } from 'react';
 
 export const AxiosProvider = ({ children }: any) => {
 
-  // using interceptors only to force their implementation in news data modification (I could implement it inside extrareducer).
+  // using interceptors only to force their implementation in news data modification (I could implement it inside extraReducers).
   axios.defaults.baseURL = API_URL;
 
-  useEffect(() => {
-    axios.interceptors.response.use(
-      (response) => {
-        if (response.data.hits) {
-          const { hits: newsList } = response.data;
+  axios.interceptors.response.use(
+    (response) => {
+      if (response.data.hits) {
+        const { hits: newsList } = response.data;
 
-          // remove data from array if author, story_title, story_url, created_at is not presented
-          response.data.hits = newsList.filter((news: Partial<INews>) =>
-            news.author && news.story_title && news.story_url && news.created_at
-          );
-
-          return response;
-        }
+        // remove data from array if authored, story_title, story_url, created_at is not presented
+        response.data.hits = newsList.filter((news: Partial<INews>) =>
+          news.author && news.story_title && news.story_url && news.created_at
+        );
 
         return response;
-      },
-      error => {
-        return Promise.reject(error);
       }
-    );
-  } , []);
+
+      return response;
+    },
+    error => {
+      return Promise.reject(error);
+    }
+  );
 
   return children;
 };
